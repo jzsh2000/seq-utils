9ff4c08517ddf2fada8cef6fbd91298fa96f8fc8
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2015-10-22 20:19:23 +0800

    Restore column sequence

--- fastq/fastqc_summary.r
+++ fastq/fastqc_summary.r
@@ -12,9 +12,16 @@ if (length(args)==0) {
 library(plyr, quietly=TRUE, warn.conflicts=FALSE)
 library(reshape2, quietly=TRUE, warn.conflicts=FALSE)
 
+# 读取所有的 summary.txt 文件
 summary <- ldply(args, read.table, sep="\t", stringsAsFactors=FALSE)
+summary.attr <- unique(summary[,2])
+
+# 用 cast 方法揉数据
 summary <- dcast(summary, V3~V2, value.var="V1")
 names(summary)[1] = "file"
+
+# 更改列顺序
+summary <- summary[c("file", summary.attr)]
 print(summary)
 
 detach('package:plyr')
