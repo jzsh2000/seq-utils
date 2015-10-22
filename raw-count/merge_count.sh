9c06aaefc3107e43300affb26a7a2b6a72401b7b
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2015-10-22 18:52:34 +0800

    The first line is also valid

--- raw-count/merge_count.sh
+++ raw-count/merge_count.sh
@@ -9,4 +9,4 @@ name_list=(${file_list[@]/*\//})
 name_list=(${name_list[@]/.count/})
 
 echo -e "\t${name_list[@]}" | tr ' ' '\t'
-paste ${file_list[@]} | awk 'NR>1{printf("%s",$1); for(i=2;i<=NF;i=i+2){ printf("\t%s",$i)} printf("%c","\n")}'
+paste ${file_list[@]} | awk '{printf("%s",$1); for(i=2;i<=NF;i=i+2){ printf("\t%s",$i)} printf("%c","\n")}'
