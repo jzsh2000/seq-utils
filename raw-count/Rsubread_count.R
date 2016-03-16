ca5b93b4abdc24c520f56a9b32a399229bc669e5
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2016-03-17 03:03:22 +0800

    Calculate raw-count value using Rsubread
    This two scripts accept mulitiple bam files, and the output may need
    further processing

---
+++ raw-count/Rsubread_count.R
@@ -0,0 +1,24 @@
+#!/usr/bin/env Rscript
+
+# Date: 2016-03-16
+# calculate raw-count value from bam file
+
+args=commandArgs(trailingOnly = TRUE)
+if (length(args) < 2) {
+    # files should be in absolute path
+    stop("Usage: $0 GTF_FILE BAM_FILE...")
+}
+
+library(Rsubread)
+
+gtf.file  <- args[1]
+bam.files <- args[-1]
+
+stopifnot(file.exists(bam.files), file.exists(gtf.file))
+
+fc <- featureCounts(bam.files, annot.ext = gtf.file,
+    isGTFAnnotationFile = TRUE, 
+    isPairedEnd = TRUE,
+    countMultiMappingReads = TRUE)
+
+write.table(fc$counts, col.names=FALSE, sep='\t')
